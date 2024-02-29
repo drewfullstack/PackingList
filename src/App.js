2867a895cacfc411d7c059ab828e1bf4c5bf75b0
Andrew Lowes <drewfullstack@gmail.com>
2024-02-29 10:45:24 +0000

    first commit - commiting all files to the repository

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Logo from "./Components/Logo";
+import Form from "./Components/Form";
+import PackingList from "./Components/PackingList";
+import Stats from "./Components/Stats";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map(
+        (item) =>
+          item.id === id
+            ? { ...item, packed: !item.packed } // If the item has the matching id, toggle the 'packed' property
+            : item // Otherwise, return the item unchanged
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to clear your items"
+    );
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div>
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Form.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+export default function Form({ onAddItems }) {
+  // Fixing the prop usage
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip? 🥰</h3>
+      <select
+        name=""
+        id=""
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => (
+          <option value={i + 1} key={i + 1}>
+            {i + 1}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
